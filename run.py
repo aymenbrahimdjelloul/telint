491a4794df707f845317184e7f433300ff682c47
Mr Aymen <92776194+aymenbrahimdjelloul@users.noreply.github.com>
2025-07-05 14:04:47 +0100

    Add files via upload

---
+++ run.py
@@ -0,0 +1,17 @@
+"""
+This code or file is part of 'Telint' project
+copyright (c) 2025 , Aymen Brahim Djelloul, All rights reserved.
+use of this source code is governed by MIT License that can be found on the project folder.
+
+@author : Aymen Brahim Djelloul
+version : 0.1
+date : 04.07.2025
+license : MIT License
+
+"""
+
+# IMPORTS
+from telint._interface import __main__
+
+if __name__ == '__main__':
+    __main__()
